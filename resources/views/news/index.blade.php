87f31dfd046c669c28fb736c47dcb4e33057b370
Denkeel <denkeelpro@gmail.com>
2021-04-03 19:40:29 +0300

    Добавил ссылки в список всех новостей на конкретную новость

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -8,17 +8,17 @@
   @foreach ($articles as $article)
   <article class="flex flex-col shadow my-4">
     <!-- Article Image -->
-    <a href="#" class="hover:opacity-75">
+    <a href=" {{ route('news/article', ['id' => $article['id']]) }} " class="hover:opacity-75">
       <img src="https://source.unsplash.com/collection/1346951/1000x500?sig=1">
     </a>
     <div class="bg-white flex flex-col justify-start p-6">
-      <a href="#" class="text-blue-700 text-sm font-bold uppercase pb-4">Technology</a>
-      <a href="#" class="text-3xl font-bold hover:text-gray-700 pb-4">{{ $article['heading']}}</a>
+      <a href=" {{ route('news/article', ['id' => $article['id']]) }} " class="text-blue-700 text-sm font-bold uppercase pb-4">Technology</a>
+      <a href=" {{ route('news/article', ['id' => $article['id']]) }} " class="text-3xl font-bold hover:text-gray-700 pb-4">{{ $article['heading']}}</a>
       <p href="#" class="text-sm pb-3">
         By <a href="#" class="font-semibold hover:text-gray-800">David Grzyb</a>, Published on April 25th, 2020
       </p>
-      <a href="#" class="pb-6">{{ $article['content']}}</a>
-      <a href="#" class="uppercase text-gray-800 hover:text-black">Continue Reading <i class="fas fa-arrow-right"></i></a>
+      <a href=" {{ route('news/article', ['id' => $article['id']]) }} " class="pb-6">{{ $article['content']}}</a>
+      <a href=" {{ route('news/article', ['id' => $article['id']]) }} " class="uppercase text-gray-800 hover:text-black">Continue Reading <i class="fas fa-arrow-right"></i></a>
     </div>
   </article>
   @endforeach
